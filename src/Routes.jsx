7c40c1a0b9fcfd1075be8b716231650f32646df5
Farhan Tahmid Sabit <ftsabit@gmail.com>
2025-11-27 01:47:58 +0600

    Running Successfully

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
-import ScrollToTop from "components/ScrollToTop";
-import ErrorBoundary from "components/ErrorBoundary";
-import NotFound from "pages/NotFound";
+import ScrollToTop from './components/ScrollToTop';
+import ErrorBoundary from './components/ErrorBoundary';
+import NotFound from './pages/NotFound';
 import SystemConfigurationSettings from './pages/system-configuration-settings';
 import PartsInventoryManagement from './pages/parts-inventory-management';
 import MaintenanceReportingAnalytics from './pages/maintenance-reporting-analytics';
